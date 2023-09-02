967cee4f7caa623af5997717874c156b0d31e2b7
platisam <platisam@gmail.com>
2023-09-03 01:06:33 +0200

    adding Heading component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { ueState, useState } from "react";
 import data from "./data";
 import List from "./List";
+import Heading from "./Heading";
 
 function App() {
   const [people, setPeople] = useState(data);
 
   return (
     <main>
+      <Heading />
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
-        <button>clear all</button>
+        <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
   );
---
+++ src/Heading.js
@@ -0,0 +1,4 @@
+const Heading = () => {
+  return <h2 className="heading">Birthday Reminder</h2>;
+};
+export default Heading;
